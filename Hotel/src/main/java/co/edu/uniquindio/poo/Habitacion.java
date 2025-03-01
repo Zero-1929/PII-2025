f5b5093d7e67e10af8a4fd2debda6ad8b5c0a1fd
Zero-1929 <142957839+zero-1929@users.noreply.github.com>
2025-03-01 10:49:43 -0500

    Creacion de Hanitacion y tipoHabitacion

---
+++ Hotel/src/main/java/co/edu/uniquindio/poo/Habitacion.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.poo;
+
+public class Habitacion {
+    String numero;
+    TipoHabitacion tipoHabitacion;
+    double precio;
+
+    public Habitacion(String numero, TipoHabitacion tipoHabitacion, double precio) {
+        this.numero = numero;
+        this.tipoHabitacion = tipoHabitacion;
+        this.precio = precio;
+    }
+}
